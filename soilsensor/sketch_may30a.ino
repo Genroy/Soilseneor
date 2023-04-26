3954d7c239cf55457801e8ad37fc683508ecc940
ThamhonChe <64970590+Genroy@users.noreply.github.com>
2023-04-26 22:46:01 +0700

    Add files via upload

---
+++ soilsensor/sketch_may30a.ino
@@ -0,0 +1,34 @@
+int sensorPin = A0; // select the input pin for the potentiometer
+int sensorValue = 0; // variable to store the value coming from the sensor
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+void setup() {
+// declare the ledPin as an OUTPUT:
+Serial.begin(9600);
+delay(500);
+  /*lcd.begin();
+  lcd.print("Hi everybody)");
+        lcd.setCursor(0, 1);
+        lcd.print("My Name's Kaz"); */
+}
+
+void loop() {
+// read the value from the sensor:
+sensorValue = analogRead(sensorPin);
+sensorValue = map(sensorValue, 0, 1023, 0, 100);
+
+lcd.begin();
+  lcd.print("sensor");
+        lcd.setCursor(0, 1);
+        lcd.print(sensorValue);
+        lcd.print(" % ");
+        delay(500);
+//Serial.print("sensor = " );
+//Serial.println(sensorValue);
+
+
+}
